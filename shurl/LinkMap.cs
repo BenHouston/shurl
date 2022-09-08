13746e0c1eaef4669525bbb7b148c6471095c5d4
BenHouston <bhouston360@hotmail.co.uk>
2022-09-08 20:08:19 +0100

    improved encoding to allow 16,777,215 unique short links

--- shurl/LinkMap.cs
+++ shurl/LinkMap.cs
@@ -44,7 +44,8 @@ namespace shurl
             else  // Generate new link
             {
                 int value = SQLTable.Rows.Count;
-                string code = Convert.ToString(value, 16).PadLeft(4,'0');
+                //string code = Convert.ToString(value, 16).PadLeft(4,'0');
+                string code = EncodeValue(value*50);
                 DataRow newrow = SQLTable.NewRow();
                 newrow.SetField("LinkID", value);
                 newrow.SetField("LongLink", longlink);
@@ -53,6 +54,24 @@ namespace shurl
                 SQLTable.Rows.Add(newrow);
                 return code;
             }
+        }
+
+        private static string EncodeValue(int value)
+        {
+            char[] Base62map = new char[64]
+                { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                'U', 'V', 'W', 'X', 'Y', 'Z', '-', '+'};
+            char[] result = { '0', '0', '0', '0' };
+            int index = 3;
+            while (value > 0)
+            {
+                int remainder = value % 64;
+                result[index] = Base62map[remainder];
+                value /= 64;
+                index--;
+            }
+            return string.Join("", result);
         }
     }
 }
